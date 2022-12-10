d3d1db5d41a10119a1d61c40efbeb15651474c2c
skwidhubz <tommaxwellparker@gmail.com>
2022-12-10 18:43:27 +1100

    js progress and CSS styling

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # coding-quiz-tmp
 A timer based quiz built with js/css &amp; html
+
---
+++ script.js
@@ -0,0 +1,60 @@
+
+// TIMER
+// TIMER()
+
+// function TIMER(){
+// setInterval(1000);
+// console.log("timer");
+
+// };
+
+// Q&A's variables
+var question1 = "What case format does JavaScript use?";
+var question2 = "What name is commonly used as the first program written in a lanugage?";
+var question3 = "A loop is defined by ____ ";
+var question4 = "We stage using the GIT____ command";
+var question5 = "JavaScript is what type of language?";
+var question6 = "The universal selector in CSS is what?";
+var answer1 = choices1[2];
+var answer2 = choices2[2];
+var answer3 = choices3[1];
+var answer4 = choices4[0];
+var answer5 = choices5[1];
+var answer6 = choices6[3];
+var choices1 = ["Giraffe", "Suit", "Camel", "Eagle"];
+var choices2 = ["Hello There", "OK Computer", "Hello World", "Initialize"];
+var choices3 = ["If", "For", "Else", "Loop"];
+var choices4 = ["Add", "Push", "Commit", "Init"];
+var choices5 = ["Descriptive", "Imperative", "Objective", "Serif"];
+var choices6 = ["$", "69", "UNI", "*"];
+
+// QUESTIONS       
+// set the question text for each new question
+// add text data to question ID per each question >> id="question-text" <<
+// Questions exist in an array variable and are called upon for each question, 1 - 6
+
+
+
+// ANSWER SELECTIONS
+
+
+// question must have a correct answer
+// when clicking on the button, checks the answer is right or wrong (truefalse) in relation to the question
+// each quiz question needs to have: question, answers X 4. each answer needs to be a true or false value. 
+// if true then add a score to localStorage...
+// key = score
+// data = 1 
+// var userScore = localStorage.setItem("score")
+//table creator 
+
+// QUIZ COMPLETE
+// find score - setItem("score", "data")
+// enter name: text box and button
+// click button and then highscores page appears feat goback or clear high scores buttons
+
+// HIGH SCORE KEEPER
+// high score record keeper
+// gets data from quiz score, stored in localStorage, and displays highest value first. 
+// var highScore = localStorage.getItem("score")
+
+

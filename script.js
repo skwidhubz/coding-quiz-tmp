b81f3bbe671b818c2bc67ba35820d55b7a2781df
skwidhubz <tommaxwellparker@gmail.com>
2022-12-10 21:10:14 +1100

    question variable array and css styling with images

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
-
 // TIMER
 // TIMER()
-
 // function TIMER(){
 // setInterval(1000);
 // console.log("timer");
-
 // };
 
 // Q&A's variables
-var question1 = "What case format does JavaScript use?";
-var question2 = "What name is commonly used as the first program written in a lanugage?";
-var question3 = "A loop is defined by ____ ";
-var question4 = "We stage using the GIT____ command";
-var question5 = "JavaScript is what type of language?";
-var question6 = "The universal selector in CSS is what?";
-var answer1 = choices1[2];
-var answer2 = choices2[2];
-var answer3 = choices3[1];
-var answer4 = choices4[0];
-var answer5 = choices5[1];
-var answer6 = choices6[3];
-var choices1 = ["Giraffe", "Suit", "Camel", "Eagle"];
-var choices2 = ["Hello There", "OK Computer", "Hello World", "Initialize"];
-var choices3 = ["If", "For", "Else", "Loop"];
-var choices4 = ["Add", "Push", "Commit", "Init"];
-var choices5 = ["Descriptive", "Imperative", "Objective", "Serif"];
-var choices6 = ["$", "69", "UNI", "*"];
+// each object(q.c.a) has an array INDEX. 0 1 2 3 4
+var questions = [
+     {
+       title: 'What case format does JavaScript use?',
+       choices: ["Giraffe", "Suit", "Camel", "Eagle"],
+       answer: "Camel",
+     },
+     {
+       title: "What name is commonly used as the first program written in a lanugage?",
+       choices: ["Hello There", "OK Computer", "Hello World", "Initialize"],
+       answer: "Hello World",
+     },
+     {
+       title: "A loop is defined by ____ ",
+       choices: ["If", "For", "Else", "Loop"],
+       answer: "For",
+     },
+     {
+       title: "We stage using the GIT____ command",
+       choices: ["Add", "Push", "Commit", "Init"],
+       answer: "Add",
+     },
+     {
+       title: "JavaScript is what type of language?",
+       choices: ["Descriptive", "Imperative", "Objective", "Serif"],
+       answer: "Imperative",
+     },
+     {
+       title: "The universal selector in CSS is what?",
+       choices: ["$", "69", "UNI", "*"],
+       answer: "*", },]
+
+
 
 // QUESTIONS       
 // set the question text for each new question
 // add text data to question ID per each question >> id="question-text" <<
 // Questions exist in an array variable and are called upon for each question, 1 - 6
+// run a for loop to cycle through the questions and the arrays of q's and a's.
+// Then we create a for loop to loop through the questions where you target the properties like title, 
+// choices, or answers to appear as you create the elements
 
+document.getElementById("#quizlist")
 
+    for(let i = 0; i < questions.length; i++) {
+    const element = questions[i];
+    listEl.textContent = questions[i];
+}
+
+// Create ordered list element
+var listEl = document.createElement("ol");
+// Create ordered list items
+// var li = document.querySelectorAll("li");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
 
 // ANSWER SELECTIONS
-
-
 // question must have a correct answer
 // when clicking on the button, checks the answer is right or wrong (truefalse) in relation to the question
 // each quiz question needs to have: question, answers X 4. each answer needs to be a true or false value. 
@@ -47,10 +74,14 @@ var choices6 = ["$", "69", "UNI", "*"];
 // var userScore = localStorage.setItem("score")
 //table creator 
 
+var score = 0;
+
 // QUIZ COMPLETE
 // find score - setItem("score", "data")
 // enter name: text box and button
 // click button and then highscores page appears feat goback or clear high scores buttons
+
+
 
 // HIGH SCORE KEEPER
 // high score record keeper
